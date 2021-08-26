8e5cfab82127daef9c6eddd2e731c430ddc45e25
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-26 10:01:41 +0800

    + first java

---
+++ javaHunts/linklist.java
@@ -0,0 +1,17 @@
+package javaHunts;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(string[] args){
+        LinkedList list= new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        System.out.println(list.contains(10));
+        System.out.println(list.indexOf(10));
+        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(Array));
+    }
+}
